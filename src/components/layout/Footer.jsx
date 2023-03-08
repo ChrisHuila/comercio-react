f13a3ce25dc7b06b5fd953d38ec0185e29328108
chrisHuila <christian.huila22@gmail.com>
2023-03-08 11:31:13 -0500

    barra de busqueda en progreso

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,11 +2,11 @@
 const Footer = () => {
     return ( 
 
-        <>
-          <div className="w-100 p-5 bg-dark" >
+        
+          <div className="w-100 p-5 footer" >
             <h3 className="text-white text-center" >FOOTER</h3>
           </div>
-        </>
+        
 
      )
 }
